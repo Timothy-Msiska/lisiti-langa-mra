cb89a10d9479432d6f1db439ca56369cc7bd20dd
Timothy-Msiska <msiskazikani@gmail.com>
2026-08-21 08:07:47 +0200

    added the MRA logo on the url icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,15 +9,15 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/mra-logo.jpeg',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+          url: '/mra-logo.jpeg',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+          url: '/mra-logo.jpeg',
         type: 'image/svg+xml',
       },
     ],
